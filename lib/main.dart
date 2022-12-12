77aaa9b84c7f77e9ccc5f799419fb07ad3634a6b
Quan Le <quanxyzabc@gmail.com>
2022-12-13 01:53:30 +0700

    Add Onboarding, Signin screen

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:rive_animation/screens/entry_point.dart';
+import 'package:rive_animation/screens/onboding/onboding_screen.dart';
+
+void main() {
+  runApp(const MyEnglishApp());
+}
+
+class MyEnglishApp extends StatelessWidget {
+  const MyEnglishApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ezi2learn',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xFFEEF1F8),
+        primarySwatch: Colors.blue,
+        fontFamily: "Intel",
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          fillColor: Colors.white,
+          errorStyle: TextStyle(height: 0),
+          border: defaultInputBorder,
+          enabledBorder: defaultInputBorder,
+          focusedBorder: defaultInputBorder,
+          errorBorder: defaultInputBorder,
+        ),
+      ),
+      home: const OnbodingScreen(),
+    );
+  }
+}
+
+const defaultInputBorder = OutlineInputBorder(
+  borderRadius: BorderRadius.all(Radius.circular(16)),
+  borderSide: BorderSide(
+    color: Color(0xFFDEE3F2),
+    width: 1,
+  ),
+);
